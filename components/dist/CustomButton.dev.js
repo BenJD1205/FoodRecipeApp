85cc53e953df205e6c6e1bada157f81e2c14f8a6
Mr.Ben <42569624+1205BEN@users.noreply.github.com>
2021-09-15 11:11:26 +0700

    Final Ui

---
+++ components/dist/CustomButton.dev.js
@@ -0,0 +1,28 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _reactNative = require("react-native");
+
+var _reactNativeLinearGradient = _interopRequireDefault(require("react-native-linear-gradient"));
+
+var _constants = require("../constants");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var CustomButton = function CustomButton(_ref) {
+  var buttonText = _ref.buttonText,
+      buttonContainerStyle = _ref.buttonContainerStyle,
+      colors = _ref.colors,
+      onPress = _ref.onPress;
+
+  if (colors.length > 0) {}
+};
+
+var _default = CustomButton;
+exports["default"] = _default;
---
+++ components/dist/index.dev.js
@@ -0,0 +1,47 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "CustomButton", {
+  enumerable: true,
+  get: function get() {
+    return _CustomButton["default"];
+  }
+});
+Object.defineProperty(exports, "TabIcon", {
+  enumerable: true,
+  get: function get() {
+    return _TabIcon["default"];
+  }
+});
+Object.defineProperty(exports, "CategoryCard", {
+  enumerable: true,
+  get: function get() {
+    return _CategoryCard["default"];
+  }
+});
+Object.defineProperty(exports, "TrendingCard", {
+  enumerable: true,
+  get: function get() {
+    return _TrendingCard["default"];
+  }
+});
+Object.defineProperty(exports, "Viewers", {
+  enumerable: true,
+  get: function get() {
+    return _Viewers["default"];
+  }
+});
+
+var _CustomButton = _interopRequireDefault(require("./CustomButton"));
+
+var _TabIcon = _interopRequireDefault(require("./TabIcon"));
+
+var _CategoryCard = _interopRequireDefault(require("./CategoryCard"));
+
+var _TrendingCard = _interopRequireDefault(require("./TrendingCard"));
+
+var _Viewers = _interopRequireDefault(require("./Viewers"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
